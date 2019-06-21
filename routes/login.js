a1050dff0f743aa62f5a7197cec2a00026f1b308
Gaurav Savanur <gauravsavanur07@gmail.com>
2019-06-21 10:46:55 +0530

    node workspace -2

---
+++ routes/login.js
@@ -0,0 +1,20 @@
+var express=require('express');
+
+var router = express.Router();
+
+
+router.post('/page',function(req,res){
+
+
+    var email=req.body.email;
+    var passwd = req.body.pass;
+    var date = req.body.date1;
+    var age = req.body.age;
+    
+     console.log("sdfghj",passwd);
+    if(email=="gauravsavanur07@gmail.com"&&passwd=="123456" && age>18){
+    res.render('page',{eml:email,passw:passwd, dat:date});
+}
+})
+
+module.exports=router;
