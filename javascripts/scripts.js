cdcdfa0a6b32e1e5941155b16891732cced839c5
Gaurav Savanur <31487324+gauravsavanur07@users.noreply.github.com>
2019-08-02 16:51:44 +0530

    Revert "Revert "reports-functionality pt 1""

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,56 +1,56 @@
 
 
-function numeric(empod){
-    var charCode = (empod.which) ? empod.which :empod.l=keyCode;
-    if(charCode != 48 && charCode >57 )
-    return false;
+// function numeric(empod){
+//     var charCode = (empod.which) ? empod.which :empod.l=keyCode;
+//     if(charCode != 48 && charCode >57 )
+//     return false;
     
-}
-function alpha(alpbts){
-    var charCode = (alpbts.which) ? alpbts.which : alpbts.keyCode;
-    if(charCode >31 && charCode <65 || charCode >90  && charCode <97 || charCode >122 &&charCode >47 || charCode <58)
-    return false;
-}
-function openingAmount(empcd){
-    var charCode = (empcd.which) ? empcd.which : empcd.keyCode;
-    if(charCode != 48 && charCode >31 && (charCode <46 || charCode >57))
-    return false;
-    return true;
-}
-function clearEditAmount(){
+// }
+// function alpha(alpbts){
+//     var charCode = (alpbts.which) ? alpbts.which : alpbts.keyCode;
+//     if(charCode >31 && charCode <65 || charCode >90  && charCode <97 || charCode >122 &&charCode >47 || charCode <58)
+//     return false;
+// }
+// function openingAmount(empcd){
+//     var charCode = (empcd.which) ? empcd.which : empcd.keyCode;
+//     if(charCode != 48 && charCode >31 && (charCode <46 || charCode >57))
+//     return false;
+//     return true;
+// }
+// function clearEditAmount(){
     
     
-    document.getElementById('acc_name ').value = "";
-    document.getElementById('number').value="";
-    document.getElementById('status').value="";
-    document.getElementById('balance').value = "";
-    document.getElementById('type') = "";
-    document.getElementById('open_date')="";
-    document.getElementById('close_date')="";
+//     document.getElementById('acc_name ').value = "";
+//     document.getElementById('number').value="";
+//     document.getElementById('status').value="";
+//     document.getElementById('balance').value = "";
+//     document.getElementById('type') = "";
+//     document.getElementById('open_date')="";
+//     document.getElementById('close_date')="";
 
 
-}
+// }
 
-function clearAddAmount(){
+// function clearAddAmount(){
     
-    document.getElementById('acc_name ').value = "";
-    document.getElementById('number').value="";
-    document.getElementById('status').value="";
-    document.getElementById('balance').value = "";
-    document.getElementById('type') = "";
-    document.getElementById('open_date')="";
-    document.getElementById('close_date')="";
-}
-function add_account_limit(){
-    var acc_type = document.getElementById("sel_acct").value;
-    if(acc_type == ""){
-        document.getElementById("sel_acct").focus();
-        return false;
-    }
-}
-   function Submit(){
+//     document.getElementById('acc_name ').value = "";
+//     document.getElementById('number').value="";
+//     document.getElementById('status').value="";
+//     document.getElementById('balance').value = "";
+//     document.getElementById('type') = "";
+//     document.getElementById('open_date')="";
+//     document.getElementById('close_date')="";
+// }
+// function add_account_limit(){
+//     var acc_type = document.getElementById("sel_acct").value;
+//     if(acc_type == ""){
+//         document.getElementById("sel_acct").focus();
+//         return false;
+//     }
+// }
+//    function Submit(){
        
-   } 
+//    } 
 
 
 
